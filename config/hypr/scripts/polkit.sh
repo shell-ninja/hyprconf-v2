230c3b9ef66447e31a739bd5829eeab9d9af2680
shell-ninja <me.js.bro@gmail.com>
2025-04-30 00:31:30 +0600

    updated polkit script

--- config/hypr/scripts/polkit.sh
+++ config/hypr/scripts/polkit.sh
@@ -2,13 +2,16 @@
 
 # Polkit possible paths files to check
 polkit=(
-  "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1"
-  "/usr/lib/polkit-kde-authentication-agent-1"
-  "/usr/lib/polkit-gnome-authentication-agent-1"
-  "/usr/libexec/polkit-gnome-authentication-agent-1"
-  "/usr/libexec/polkit-mate-authentication-agent-1"
-  "/usr/lib/x86_64-linux-gnu/libexec/polkit-kde-authentication-agent-1"
-  "/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1"
+    "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1"
+    "/usr/libexec/hyprpolkitagent"
+    "/usr/lib/hyprpolkitagent"
+    "/usr/lib/hyprpolkitagent/hyprpolkitagent"
+    "/usr/lib/polkit-kde-authentication-agent-1"
+    "/usr/lib/polkit-gnome-authentication-agent-1"
+    "/usr/libexec/polkit-gnome-authentication-agent-1"
+    "/usr/libexec/polkit-mate-authentication-agent-1"
+    "/usr/lib/x86_64-linux-gnu/libexec/polkit-kde-authentication-agent-1"
+    "/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1"
 )
 
 executed=false  # Flag to track if a file has been executed

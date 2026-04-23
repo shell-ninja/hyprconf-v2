6c6c733f4b4e48533e8d8d945d191d13a8f1bbd4
shell-ninja <me.js.bro@gmail.com>
2026-04-23 11:02:32 +0600

    added waybar reload

--- config/hypr/scripts/Refresh.sh
+++ config/hypr/scripts/Refresh.sh
@@ -2,12 +2,13 @@
 # Refresh.sh — Restart notification daemon and reload Hyprland.
 
 # Kill running daemons
-_ps=(swaync rofi)
+_ps=(swaync rofi waybar)
 for _prs in "${_ps[@]}"; do
     pidof "${_prs}" &>/dev/null && pkill -SIGTERM "${_prs}"
 done
 
 sleep 0.4
+waybar &
 swaync &
 
 hyprctl reload
